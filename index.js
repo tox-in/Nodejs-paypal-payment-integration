3032b04634aba0e57fd5231423ce8b79fcfc5666
tox-in <habimanat78@gmail.com>
2024-09-28 15:26:12 +0200

    for a product

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+const paypal = require('paypal-rest-sdk');
+
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': process.env.PAYPAL_CLIENT_ID,
+    'client_secret': process.env.PAYPAL_CLIENT_SECRET
+});
 
 const app = express();
 
-app.set('view engine','ejs')
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:3000/success",
+            "cancel_url": "http://localhost:3000/cancel"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Red Sox Hat",
+                    "sku": "001",
+                    "price": "25.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "25.00"
+            },
+            "description": "Hat for the best team ever"
+        }]
+    };
 
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            console.error(error);
+            res.send('Error occurred');
+        } else {
+            for (let i = 0; i < payment.links.length; i++) {
+                if (payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+});
 
-app.listen(3000, ()=>console.log('Server started on http://localhost:3000'));
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
